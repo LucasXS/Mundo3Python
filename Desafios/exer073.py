fa31ed8ee1dd9da8381c8d75ca1809d6777511c3
lucas <contato_lucassantos@outlook.com>
2021-03-28 23:10:21 -0300

    Desafio 073 - Tabela do Campeonato Brasileiro
    Minha versão do programa.

--- Desafios/exer073.py
+++ Desafios/exer073.py
@@ -1,7 +1,7 @@
 # Crie uma tupla preenchida com os 20 primeiros colocados na Tabela do CBF, na ordem de colocação. Depois mostre:
 # A - Apenas os 5 primeiros colocados. # B - Os últimos 4 colocados
 # C - Uma lista com os times em ordem alfabética. # D - Em que posição na tabela está op time do Bahia.
-
+# 
 times = ('SÃO PAULO', 'FLAMENGO', 'INTERNACIONAL', 'GRÊMIO',
          'PALMEIRAS', 'ATLÉTICO-MG', 'ATHLETICO', 'CRUZEIRO', 'BOTAFOGO',
          'SANTOS', 'BAHIA', 'FLUMINENSE', 'CORINTHIANS', 'CHAPECOENSE',
