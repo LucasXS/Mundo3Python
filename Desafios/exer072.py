c4db23c73bb3436844b7feebaaf82dbd2e855c4b
lucas <contato_lucassantos@outlook.com>
2021-03-28 23:11:55 -0300

    Desafio 072 - Trabalhando com TUPLA,  0 - 20

--- Desafios/exer072.py
+++ Desafios/exer072.py
@@ -3,7 +3,6 @@
 
 tupla = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez',
            'onze', 'doze', 'treze', 'catorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte',)
-
 num = int(input('Digite um número entre 0 e 20: '))
 while num != len(tupla):
     if num < 0 or num > 20:
