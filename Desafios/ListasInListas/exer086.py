9ff402d61801137dff2f419a132ac028c4bca805
Lucas Santos <contato_lucassantos@outlook.com>
2021-04-30 23:38:27 -0300

    Matriz de 3x3

---
+++ Desafios/ListasInListas/exer086.py
@@ -0,0 +1,14 @@
+"""DESAFIO 086 - Crie um programa que crie uma MATRIZ de dimensão 3x3 e preencha com valores lidos pelo teclado.
+No final, mostre a MATRIZ na tela, com a formação correta."""
+
+# cria uma Matriz vazia de 3 linhas
+matriz = [[],[],[]]
+for i in range(0, 3):       # LINHAS
+    for j in range(0, 3):   # COLUNAS
+        # vai adicionados números de entrada na LINHA
+        matriz[i].append(int(input(f'Digite o valor para [{i}, {j}]: ')))
+
+for i in range(0, 3):
+    for j in range(0, 3):
+        print(f'[{matriz[i][j]:^5}]', end='')
+    print()
