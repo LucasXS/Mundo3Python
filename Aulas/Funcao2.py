1d33624e921a910f6073f32cc79cd6db7aa177c3
Lucas Santos <contato_lucassantos@outlook.com>
2021-05-27 23:54:21 -0300

    Create Funcao2.py

---
+++ Aulas/Funcao2.py
@@ -0,0 +1,13 @@
+def somar(a=0, b=0, c=0):
+
+    """
+    :param a: O primeiro valor
+    :param b: O segundo valor
+    :param c: O terceiro valor
+    :return: não retorna nada
+    """
+    s = a + b + c
+    print(f'A soma vale {s}')
+
+
+somar(5, 5, 4, 8)
